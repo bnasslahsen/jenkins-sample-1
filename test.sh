1b648876e9db8959bbf65093cdcf6d0416a993de
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-31 15:35:30 +0200

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-CONJUR_SESSION_TOKEN= $(curl -k --location --request POST "${CONJUR_HOST}/authn/devsecops/admin/authenticate" \
---header "Content-Type: application/json" \
---header "Accept-Encoding: base64" \
---data-raw "${CONJUR_API_KEY}" | head -n 1| cut -d $' ' -f2)
 
+CONJUR_SESSION_TOKEN=$(curl -k --location --request POST "${CONJUR_HOST}/authn/devsecops/admin/authenticate" --header "Content-Type: application/json" --header "Accept-Encoding: base64" --data-raw "${CONJUR_API_KEY}" | head -n 1| cut -d $' ' -f2
 
+CONJUR_SECRET= curl -k --location --request GET "${CONJUR_HOST}/secrets/devsecops/variable/ci/jenkins/secrets/github_private_key" --header 'Authorization: Token token="$CONJUR_SESSION_TOKEN"'
+
+echo $CONJUR_SECRET
