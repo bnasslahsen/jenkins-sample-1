356c4b299e8dc18a62ceb9c57e1c6abd5578e803
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-31 15:05:33 +0200

    test

--- test.sh
+++ test.sh
@@ -4,7 +4,7 @@
 CONJUR_SESSION_TOKEN= $(curl -k --location --request POST ${CONJUR_HOST}/authn/devsecops/admin/authenticate \
 --header 'Content-Type: application/json' \
 --header 'Accept-Encoding: base64' \
---data-raw '${CONJUR_API_KEY}')
+--data-raw ${CONJUR_API_KEY})
 
 echo $CONJUR_SESSION_TOKEN
 
