062b059c2c57ab9648548b9124d0b5f7046ab929
Badr.NassLahsen <badr.nasslahsen@gmail.com>
2022-08-31 15:01:48 +0200

    test

--- test.sh
+++ test.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-echo "${CONJUR_HOST}/authn/devsecops/admin/authenticate"
 
-curl -k --location --request POST ${CONJUR_HOST}/authn/devsecops/admin/authenticate \
+CONJUR_SESSION_TOKEN=curl -k --location --request POST ${CONJUR_HOST}/authn/devsecops/admin/authenticate \
 --header 'Content-Type: application/json' \
 --header 'Accept-Encoding: base64' \
 --data-raw '${CONJUR_API_KEY}'
+
+echo $CONJUR_SESSION_TOKEN
 
